3d5a387881614952c8e97c1dd750118386cd180c
David Gonzalez <josedavidgonzalezt@gmail.com>
2024-06-12 15:28:27 -0300

    validate results

--- src/components/organisms/ReservationList.tsx
+++ src/components/organisms/ReservationList.tsx
@@ -1,33 +1,38 @@
-"use client"
-import React from 'react';
-import { Grid, Box, Typography } from '@mui/material';
-import ReservationCard from '../molecules/ReservationCard';
-import useInfiniteScroll from '../../hooks/useInfiniteScroll';
-import { Restaurant } from '@/types';
+"use client";
+import React from "react";
+import { Grid, Box, Typography } from "@mui/material";
+import ReservationCard from "../molecules/ReservationCard";
+import { Restaurant } from "@/types";
 
 interface ReservationListProps {
   results: Restaurant[];
   fetchMoreResults: () => void;
-  handleSearch: () => Promise<void>
+  handleSearch: () => Promise<void>;
 }
 
-const ReservationList: React.FC<ReservationListProps> = ({ results, fetchMoreResults, handleSearch }) => {
-  const [isFetching, setIsFetching] = useInfiniteScroll(fetchMoreResults);
+const ReservationList: React.FC<ReservationListProps> = ({
+  results,
+  handleSearch,
+}) => {
 
   return (
     <Box my={4}>
       <Grid container spacing={2}>
-        {results.map((result, index) => (
-          <Grid item xs={12} md={6} key={index}>
-            <ReservationCard restaurant={result} handleSearch={handleSearch} />
-          </Grid>
-        ))}
+        {results.length ? (
+          results.map((result, index) => (
+            <Grid item xs={12} md={6} key={index}>
+              <ReservationCard
+                restaurant={result}
+                handleSearch={handleSearch}
+              />
+            </Grid>
+          ))
+        ) : (
+          <Box display="flex" justifyContent="center" my={2}>
+            <Typography>There is not results</Typography>
+          </Box>
+        )}
       </Grid>
-      {isFetching && (
-        <Box display="flex" justifyContent="center" my={2}>
-          <Typography>Cargando más resultados...</Typography>
-        </Box>
-      )}
     </Box>
   );
 };
